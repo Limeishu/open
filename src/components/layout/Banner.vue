6bcbc21ad17229b87723cb13e00b45066714b79f
Aries Cs <aries0d0f@gmail.com>
2018-02-19 10:38:36 +0800

    Add down arrow.

--- src/components/layout/Banner.vue
+++ src/components/layout/Banner.vue
@@ -6,6 +6,7 @@
     <header>
       <h1>開源×文史</h1>
       <p><span class="tag">藝術</span><span>，可以</span><span class="tag">不只是藝術</span><span>｜</span><span class="tag">More Than Art.</span></p>
+      <div class="next" v-scroll-to="'#how, 0'"><img src="@/assets/images/down-arrow.svg" alt=""></div>
     </header>
   </div>
 </template>
@@ -19,9 +20,9 @@
     methods: {
       animate (index) {
         let height = this.scrollHeight < -this.deviceHeight ? -this.deviceHeight : this.scrollHeight
-        let x = -Math.cos(index / 30 * Math.PI) * 600
-        let y = 2 * height * Math.cos(index)
-        let z = Math.sin(index / 30 * Math.PI) * 600 * Math.cos(index)
+        let x = parseInt(-Math.cos(index / 30 * Math.PI) * 600)
+        let y = parseInt(2 * height * Math.cos(index))
+        let z = parseInt(Math.sin(index / 30 * Math.PI) * 600 * Math.cos(index))
         return `${x}px, ${y}px, ${z}px`
       }
     }
