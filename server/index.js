d88d43ac868409a0801a63f1a8db6541f1677d24
Aries Cs <aries0d0f@gmail.com>
2018-02-19 11:38:47 +0800

    Add static server.

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const app         = new (require('koa'))()
+const send        = require('koa-send')
+const path        = require('path')
+const config      = require('./config.json')
+
+app.use(require('koa-static')(path.join(__dirname, '/../dist/')))
+app.use(async (ctx, next) => {
+  await send(ctx, (path.join('/../../webroot/dist/index.html')))
+})
+app.listen(config.port)
+console.log(`Start server on port ${config.port}.`)
