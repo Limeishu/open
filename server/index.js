bd037ce4c34b84f14a6118c5b4fd818805317f7e
Aries Cs <aries0d0f@gmail.com>
2018-02-19 12:04:28 +0800

    Fix error handling.

--- server/index.js
+++ server/index.js
@@ -1,11 +1,10 @@
 const app         = new (require('koa'))()
-const send        = require('koa-send')
 const path        = require('path')
 const config      = require('./config.json')
 
 app.use(require('koa-static')(path.join(__dirname, '/../dist/')))
 app.use(async (ctx, next) => {
-  await send(ctx, (path.join('/../../webroot/dist/index.html')))
+  await ctx.redirect('https://limeishu.org.tw/error/404')
 })
 app.listen(config.port)
 console.log(`Start server on port ${config.port}.`)
